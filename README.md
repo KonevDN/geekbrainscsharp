54fd6e0e7337c778e657e08ffe427b9fd6f66121
daniilkonef <daniilkonef@yandex.ru>
2022-01-18 23:42:54 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# geekbrainscsharp
+ 
---
+++ lesson1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Int32 x2 = 0;
+            String name = null;
+            Console.WriteLine("Напишите как вас зовут:");
+            name = Console.ReadLine();
+            Console.WriteLine($" Привет {name}, сегодня {DateTime.Now} ");
+            Console.ReadLine();
+        }
+    }
+}
