79f08e7b9ed59efc634799a06e913aebb131c7b8
Даниил Конев <KonevDN@ya.ru>
2022-06-05 14:59:21 +0300

    +

---
+++ lesson5/Project3/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+// УРОК5, ЗАДАЧА3 Ввести с клавиатуры произвольный набор чисел (0...255) и записать их в бинарный файл.
+
+string int1= Console.ReadLine(); // я пробовал Console.Read() но так почему то результат читается неверный 
+byte b1= Convert.ToByte(int1);
+byte[] b2 = new byte[1]{b1};
+//b2[0] = b1;
+File.WriteAllBytes("file1.bin", b2);
+byte[] b3 = File.ReadAllBytes("file1.bin");
+Console.WriteLine(b3[0]);
+
+//Console.WriteLine("Проверка:");
+//byte[] b4 = File.ReadAllBytes("file1.bin");
+//Console.WriteLine(b4[0]);
