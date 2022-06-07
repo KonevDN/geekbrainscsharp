98cf1983466f99def8e6ffb888711da3e654aad5
Даниил Конев <KonevDN@ya.ru>
2022-06-08 00:07:39 +0300

    В принципе уже сделал домашку.

---
+++ lesson6/Project1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Mail;
+//using static TaskManager.Менеджер;
+
+namespace TaskManager 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Менеджер.ВывестиВсеПроцессыНаЭкран();
+                Менеджер.СпроситьЮзераКакойПроцессУбить();
+                Менеджер.УбитьУказанныйПроцесс();
+                Менеджер.ПроверитьЧтоПроцессУбитУспешно();
+                Console.Clear();
+                
+
+            }
+        }
+    }
+}
---
+++ lesson6/Project2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Project2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Process process = new Process();
+            process.StartInfo.FileName =
+                @"D:\Projects\geekbrainscsharp\lesson6\Project1\bin\Debug\netcoreapp3.1\Project1.exe";
+            process.StartInfo.Arguments = "1 2 3";
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.Start();
+            string output = process.StandardOutput.ReadToEnd();
+            Console.WriteLine(output);
+            process.WaitForExit();
+            Console.WriteLine(process.ExitCode);
+        }
+    }
+}
