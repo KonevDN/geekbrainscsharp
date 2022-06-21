4fe6a285fb27dfe4e10087c57cad4b6d69896d78
daniilkonef <daniilkonef@yandex.ru>
2022-06-21 23:47:01 +0300

    Конец дня

--- lesson9/Project1/Библиотеки/Собственные/Окно/ПараметрыОкна.cs
+++ lesson9/Project1/Библиотеки/Собственные/Окно/ПараметрыОкна.cs
@@ -4,42 +4,48 @@ namespace БиблиотекаОкно
 {
     public static class ПараметрыОкна
     {
-        private static UInt16 ТочкаСлева = 0;
-        private static UInt16 ТочкаСверху = 0;
-        private static UInt16 ШиринаЗоны = 120;
-        private static UInt16 ВысотаЗоны = 30;
-        private static UInt16 ШиринаОкна = ШиринаЗоны;
-        private static UInt16 ВысотаОкна = ВысотаЗоны;
+        private static Int32 ТочкаСлева = 0;
+        private static Int32 ТочкаСверху = 0;
+        private static Int32 ШиринаЗоны1 = 120;
+        private static Int32 ВысотаЗоны1 = 10;
+        private static Int32 ВысотаЗоны2 = 30;
+        private static Int32 ШиринаОкна = ШиринаЗоны1;
+        private static Int32 ВысотаОкна = ВысотаЗоны1*5;
+        private static Int32 _ширинаБуфера = ШиринаЗоны1;
+        private static Int32 _высотаБуфера = ВысотаЗоны1*5;
 
-        public static UInt16 ВзятьЖелаемуюШиринуЗоны
+        public static Int32 ВзятьЖелаемуюШиринуЗоны1
         {
-            get { return ШиринаЗоны; }
+            get { return ШиринаЗоны1; }
         }
 
-        public static UInt16 ВзятьЖелаемуюВысотуЗоны
+        public static Int32 ВзятьЖелаемуюВысотуЗоны1
         {
-            get { return ВысотаЗоны; }
+            get { return ВысотаЗоны1; }
         }
 
-        public static UInt16 ВзятьЖелаемуюШиринуОкна
+        public static Int32 ВзятьНастроеннуюШиринуОкна
         {
             get { return ШиринаОкна; }
         }
 
-        public static UInt16 ВзятьЖелаемуюВысотуОкна
+        public static Int32 ВзятьНастроеннуюВысотуОкна
         {
             get { return ВысотаОкна; }
         }
 
-        public static UInt16 ВзятьЖелаемуюНачальнуюТочкуСлева
+        public static Int32 ВзятьЖелаемуюНачальнуюТочкуСлева
         {
             get { return ТочкаСлева; }
         }
 
-        public static UInt16 ВзятьЖелаемуюНачальнуюТочкуСверху
+        public static Int32 ВзятьЖелаемуюНачальнуюТочкуСверху
         {
             get { return ТочкаСверху; }
         }
 
+        public static int ШиринаБуфера => _ширинаБуфера;
+
+        public static int ВысотаБуфера => _высотаБуфера;
     }
 }
