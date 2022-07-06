0caf82218fc42cbc07e8ad5040e5b5ca31633160
daniilkonef <daniilkonef@yandex.ru>
2022-07-06 19:20:24 +0300

    Заработали пара методов РаспечататьСписокКаталогов и РаспечататьСписокФайлов

---
+++ lesson5/Project4/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+
+namespace Project4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            // Directory, File, FileInfo, DirectoryInfo
+
+            DirectoryInfo directoryInfo = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory);
+
+            ////Console.WriteLine("FullName: {0}", directoryInfo.FullName);
+            //Console.WriteLine($"Полный адрес каталога: {directoryInfo.FullName}");
+            //Console.WriteLine("Name: {0}", directoryInfo.Name);
+            //Console.WriteLine("Parent: {0}", directoryInfo.Parent);
+            //Console.WriteLine("Creation: {0}", directoryInfo.CreationTime);
+            //Console.WriteLine("Attributes: {0}", directoryInfo.Attributes.ToString());
+            //Console.WriteLine("Root: {0}", directoryInfo.Root);
+
+            //Console.WriteLine();
+
+            Console.WriteLine($"Родитель: {directoryInfo.Parent}"); 
+            РаспечататьСписокКаталогов(new DirectoryInfo(directoryInfo.FullName), "", true);
+
+
+            Console.ReadKey(true);
+        }
+
+        static void РаспечататьСписокКаталогов(DirectoryInfo НачальнаяДиректория, string ОтступВСтрокеКаталога, bool ЭтоПоследняяДиректория)
+        {
+            
+            Console.Write(ОтступВСтрокеКаталога);
+            Console.Write(ЭтоПоследняяДиректория ? "└─" : "├─");
+            ОтступВСтрокеКаталога = ОтступВСтрокеКаталога + (ЭтоПоследняяДиректория ? " " : "│ ");
+            Console.WriteLine("Каталог: " + НачальнаяДиректория.Name);
+            РаспечататьСписокФайлов(НачальнаяДиректория, ОтступВСтрокеКаталога);
+
+
+            DirectoryInfo[] СписокКаталогов = НачальнаяДиректория.GetDirectories();
+            for (int i = 0; i < СписокКаталогов.Length; i++)
+            {
+                РаспечататьСписокКаталогов(СписокКаталогов[i], ОтступВСтрокеКаталога, i == СписокКаталогов.Length - 1);
+            }
+
+        }
+
+        static void РаспечататьСписокФайлов(DirectoryInfo ТекущаяДиректория, String ОтступВСтрокеКаталога)
+        {
+            String ОтступВСтрокеФайла = ОтступВСтрокеКаталога + "├─";
+            FileInfo[] СписокФайлов = ТекущаяДиректория.GetFiles();
+            for (Int32 index = 0; index < СписокФайлов.Length; index++)
+            {
+                Console.Write(ОтступВСтрокеФайла);
+                Console.WriteLine("Файл: " + СписокФайлов[index].Name);
+            }
+        }
+
+
+    }
+}
---
+++ lesson9/Project1/Обработка.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using БиблиотекаОкно;
+using БиблиотекаОперации;
+using БиблиотекаПаспорт;
+
+
+namespace FireManagerConsole
+{
+    public static class Обработка
+    {
+        static void Main(string[] args)
+        {
+            System.Reflection.Assembly Сборка = System.Reflection.Assembly.GetExecutingAssembly(); // Получим информацию по текущей сборки
+            System.Diagnostics.FileVersionInfo FileManager = System.Diagnostics.FileVersionInfo.GetVersionInfo(Сборка.Location); // Получим информацию по текущей версии сборки
+
+#if DEBUG
+            String ЗаголовокОкна = $"{FileManager.FileDescription} " + $"" + $"{FileManager.FileVersion} {"[DEBUG]"} by {FileManager.CompanyName} © 06/2022";
+            Console.Title = ЗаголовокОкна;
+#else
+            String ЗаголовокОкна = $"{FileManager.FileDescription} {FileManager.FileVersion} by {FileManager.CompanyName}" ;
+            Console.Title = ЗаголовокОкна; 
+#endif
+                
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            ГрафикаОкна.УстановитьРазмерОкна(ПараметрыОкна.ШиринаОкна, ПараметрыОкна.ВысотаОкна); 
+            ПозицияОкна.ЦентрироватьОкно(ЗаголовокОкна);
+            ГрафикаОкна.ОтрисоватьЗонуОкна(ПараметрыЗоны.ТочкаСлеваДляЗоны1, ПараметрыЗоны.ТочкаСверхуДляЗоны1, ПараметрыЗоны.ШиринаЗоны1, ПараметрыЗоны.ВысотаЗоны1);
+            ГрафикаОкна.ОтрисоватьЗонуОкна(ПараметрыЗоны.ТочкаСлеваДляЗоны2, ПараметрыЗоны.ТочкаСверхуДляЗоны2, ПараметрыЗоны.ШиринаЗоны2, ПараметрыЗоны.ВысотаЗоны2);
+            ГрафикаОкна.ОтрисоватьЗонуОкна(ПараметрыЗоны.ТочкаСлеваДляЗоны3, ПараметрыЗоны.ТочкаСверхуДляЗоны3, ПараметрыЗоны.ШиринаЗоны3, ПараметрыЗоны.ВысотаЗоны3);
+
+            while (true)
+            {
+                ГрафикаОкна.ЗаполнитьЗонуОкна3(ПаспортПриложения.ДиректорияЗапуска);
+                ОбработкаСтрокиВвода.ОбработатьВводПользователя(ПараметрыЗоны.ШиринаЗоны3);
+            }
+            
+            // урок 9 1:44
+        }
+
+
+        static void DrawTree(DirectoryInfo ТекущаяДиректория, int ЗапрошеннаяСтраница)
+        {
+            StringBuilder Дерево = new StringBuilder();
+            GetTree(Дерево, ТекущаяДиректория, "", true);
+            //DrawWindow(0, 0, WINDOW_WIDTH, 18);
+            ГрафикаОкна.ОтрисоватьЗонуОкна(ПараметрыЗоны.ТочкаСлеваДляЗоны1, ПараметрыЗоны.ТочкаСверхуДляЗоны1, ПараметрыЗоны.ШиринаЗоны1, ПараметрыЗоны.ВысотаЗоны1);
+
+            (Int32 currentLeft, Int32 currentTop) = ВзятьТекущиеКоординатыКурсора();
+            Int32 КоличествоСтрокВЗонеВывода = 16;
+
+            string[] ВсеСтрокиИзДерева = Дерево.ToString().Split('\n'); 
+            Int32 НеобходимоеКоличествоСтраницДляВывода = (ВсеСтрокиИзДерева.Length + КоличествоСтрокВЗонеВывода - 1) / КоличествоСтрокВЗонеВывода;
+            if (ЗапрошеннаяСтраница > НеобходимоеКоличествоСтраницДляВывода) ЗапрошеннаяСтраница = НеобходимоеКоличествоСтраницДляВывода;
+
+            for (Int32 i = (ЗапрошеннаяСтраница - 1) * КоличествоСтрокВЗонеВывода, counter = 0; i < ЗапрошеннаяСтраница * КоличествоСтрокВЗонеВывода; i++, counter++)
+            {
+                if (ВсеСтрокиИзДерева.Length - 1 > i)
+                {
+                    Console.SetCursorPosition(currentLeft + 1, currentTop + 1 + counter); 
+                    Console.WriteLine(ВсеСтрокиИзДерева[i]); 
+                }
+            }
+
+            // Отрисуем footer
+            string footer = $"╡ {ЗапрошеннаяСтраница} of {НеобходимоеКоличествоСтраницДляВывода} ╞";
+            Console.SetCursorPosition(WINDOW_WIDTH / 2 - footer.Length / 2, 17); 
+            Console.WriteLine(footer);
+
+
+            (Int32 Слева, Int32 Справа) ВзятьТекущиеКоординатыКурсора()
+            {
+                return (Console.CursorLeft, Console.CursorTop);
+            }
+
+        }
+
+
+
+        static void GetTree(StringBuilder Дерево, DirectoryInfo ТекущаяДиректория, string Отступ, bool ЭтоПоследняяДиректория)
+        {
+            Дерево.Append(Отступ);
+            if (ЭтоПоследняяДиректория)
+            {
+                Дерево.Append("└─");
+                Отступ += "  ";
+            }
+            else
+            {
+                Дерево.Append("├─");
+                Отступ += "│ ";
+            }
+
+            Дерево.Append($"{ТекущаяДиректория.Name}\n");
+
+
+            FileInfo[] subFiles = ТекущаяДиректория.GetFiles();
+            for (int i = 0; i < subFiles.Length; i++)
+            {
+                if (i == subFiles.Length - 1)
+                {
+                    Дерево.Append($"{Отступ}└─{subFiles[i].Name}\n");
+                }
+                else
+                {
+                    Дерево.Append($"{Отступ}├─{subFiles[i].Name}\n");
+                }
+            }
+
+
+            DirectoryInfo[] subDirects = ТекущаяДиректория.GetDirectories();
+            for (int i = 0; i < subDirects.Length; i++)
+                GetTree(Дерево, subDirects[i], Отступ, i == subDirects.Length - 1);
+        }
+
+
+
+    }
+}
